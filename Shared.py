3d77323b6323f92c7220af048528fd3628e846f0
Kevin Moore <kevin@thinkpixellab.com>
2010-10-17 11:42:35 -0700

    Factored out a simple proc runner

--- Shared.py
+++ Shared.py
@@ -24,22 +24,22 @@ def get_tmp_file_name(source_file_name):
   return os.path.join(tmp_dir, name)
 
 def run_process_file_command(command_func):
-  logging.basicConfig(format=' * %(message)s', level=logging.INFO)
   args, tmp_file, out_file = command_func()
+  stdoutdata = run_process(args)
+  logging.info("Moving temp file to '%s'", out_file)
+  remove_if_exists(out_file)
+  os.rename(tmp_file, out_file)
+
+def run_process(args):
+  logging.basicConfig(format=' * %(message)s', level=logging.INFO)
   logging.info('* * * * *')
   logging.info('Requested command: %s', ' '.join(args))
-  proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+  proc = subprocess.Popen(args, stdout=sys.stdout)
   logging.info('Running...')
-  (stdoutdata, stderrdata) = proc.communicate()
-  if proc.returncode != 0:
-    logging.error('Command failed.')
+  proc.communicate()
+  if(proc.returncode != 0):
+    logging.error('Command failed')
     sys.exit(1)
-  else:
-    sys.stdout.write(stdoutdata)
-    logging.info('Command succeeded')
-    logging.info("Moving temp file to '%s'", out_file)
-    remove_if_exists(out_file)
-    os.rename(tmp_file, out_file)
 
 def find_files(directory, pattern):
     for root, dirs, files in os.walk(directory):
