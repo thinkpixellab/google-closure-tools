b49896fa66415c7b2b4e7cd31ead5fbb8fb43581
Kevin Moore <kevin@thinkpixellab.com>
2011-06-10 14:29:36 -0700

    Closure compiler to svn1171

--- Closure.py
+++ Closure.py
@@ -4,7 +4,7 @@ import glob
 from Shared import *
 import HtmlPost
 
-jar_path = os.path.join(os.path.dirname(__file__), 'compiler-svn978.jar')
+jar_path = os.path.join(os.path.dirname(__file__), 'compiler-svn1171.jar')
 
 tmp_dir = 'tmp'
 
